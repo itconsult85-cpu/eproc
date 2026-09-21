447915de03f976cc9fc3d5f37338c5efcfa532e8
itconsult85-cpu <2.46589482e+08+itconsult85-cpu@users.noreply.github.com>
2026-09-21 19:25:55 +0000

    Load jQuery before DataTables

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -217,6 +217,7 @@
         </main>
         <footer class="app-footer"><strong>Eprocurement</strong> &mdash; internal workspace</footer>
     </div>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
     </script>
